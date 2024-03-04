bd3c55ca685f7f48a320e418b2fe10cd64d65b6e
devdruh <dev.drewragadio@gmail.com>
2024-03-04 14:59:44 -0500

    get all users - api

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js'
 import connectToMongoDb from './db/connectToMongoDB.js';
+import userRoutes from './routes/user.routes.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
 app.use('/api/messages', messageRoutes);
 
 // app.get('/', (req, res) => {
