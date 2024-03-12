9fe6f2ebd16fd5491ebaf297eace9a9e743362c7
devdruh <dev.drewragadio@gmail.com>
2024-03-12 19:30:01 -0400

    display users

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
 import Conversation from './Conversation'
+import useGetConversations from '../../hooks/useGetConversations';
+import { getRandomEmoji } from '../../utils/emoji';
 
 const Conversations = () => {
+    
+    const { loading, conversations } = useGetConversations();
+
     return (
-        <div>
-            <Conversation/>
-            <Conversation/>
-            <Conversation/>
-            <Conversation/>
-            <Conversation/>
-            <Conversation/>
+        <div className='py-2 flex flex-col overflow-auto'>
+            {
+                conversations.map((conversation, index) => (
+                    <Conversation
+                        key={conversation._id}
+                        conversation={conversation}
+                        emoji={getRandomEmoji()}
+                        lastIndex={index === conversations.length - 1 } />
+                ))
+            }
+            {
+                loading ? <span className='loading loading-spinner mx-auto'></span> : null
+            }
         </div>
-  )
+    )
 }
 
-export default Conversations
+export default Conversations;
