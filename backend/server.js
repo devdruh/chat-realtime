8a52c1acb5dae022f64ca1814152d2b3886f32ea
devdruh <dev.drewragadio@gmail.com>
2024-03-20 21:00:33 -0400

    backend : socket io -  login users

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import { app, server } from './socket/socket.js'
 
 import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js'
 import connectToMongoDb from './db/connectToMongoDB.js';
 import userRoutes from './routes/user.routes.js';
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -24,7 +24,7 @@ app.use('/api/messages', messageRoutes);
 //     res.send("hellooo")
 // })
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     // console.log();
     connectToMongoDb();
     console.log(`Server running on port ${PORT}`)
