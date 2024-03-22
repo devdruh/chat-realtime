f68c4b484ca1d6c356532997b3894b1249c0fdc5
devdruh <dev.drewragadio@gmail.com>
2024-03-22 16:06:11 -0400

    deployment config

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
@@ -9,6 +10,7 @@ import connectToMongoDb from './db/connectToMongoDB.js';
 import userRoutes from './routes/user.routes.js';
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -18,6 +20,12 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/messages', messageRoutes);
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
+});
 
 // app.get('/', (req, res) => {
 //     // root route
