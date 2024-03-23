a6c9bd779aa121466e8193eca164aed5c47b2752
devdruh <dev.drewragadio@gmail.com>
2024-03-23 13:33:19 -0400

    update server  index

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-// import path from 'path';
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
@@ -10,7 +10,7 @@ import connectToMongoDb from './db/connectToMongoDB.js';
 import userRoutes from './routes/user.routes.js';
 
 const PORT = process.env.PORT || 5000;
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -21,16 +21,16 @@ app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/messages', messageRoutes);
 
-// app.use(express.static(path.join(__dirname, '/frontend/dist')));
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
-// });
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
+});
 
-app.get('/', (req, res) => {
-    // root route
-    res.send("Hello World!")
-})
+// app.get('/', (req, res) => {
+//     // root route
+//     res.send("Hello World!")
+// })
 
 server.listen(PORT, () => {
     // console.log();
